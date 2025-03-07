b95782316581accf964ec36a225c09e67347471e
SystemDevps <systemeveloperscompany@gmail.com>
2025-03-06 19:12:06 -0600

    Token

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metabot.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///metabot.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
@@ -19,31 +19,58 @@ class Log(db.Model):
 with app.app_context():
     db.create_all()
 
-    prueba1 = Log(texto='Prueba 1')
-    prueba2 = Log(texto='Prueba 2')
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
 
 def ordenar_por_fecha_y_hora(registros):
     return sorted(registros, key=lambda x: x.fecha_y_hora, reverse=True)
 
-@app.route('/')
+
+@app.route("/")
 def index():
 
     registros = Log.query.all()
     registros_ordenados = ordenar_por_fecha_y_hora(registros)
-    return render_template('index.html',registros=registros_ordenados)
+    return render_template("index.html", registros=registros_ordenados)
 
     messages_log = []
 
-    def agregar_messages_log(texto):
-        messages_log.append(texto)
 
-        nuevo_registro = Log(texto=texto)
-        db.session.add(nuevo_registro)
-        db.session.commit()
+def agregar_messages_log(texto):
+    messages_log.append(texto)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    nuevo_registro = Log(texto=texto)
+    db.session.add(nuevo_registro)
+    db.session.commit()
+
+
+TOKEN_SYSTEMDEVELOPERS = "SYSTEMDEVELOPERS"
+
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        challenge = verify_token(request)
+        return challenge
+    elif request.method == "POST":
+        reponse = recibir_mensaje(request)
+        return reponse
+
+
+def verify_token(req):
+    token=req.args.get("hub.verify_token")
+    challenge=req.args.get("hub.challenge")
+
+    if challenge and token == TOKEN_SYSTEMDEVELOPERS:
+        return challenge
+    else:
+        return jsonify({"error": "Token Invalido"}),401
+
+
+def recibir_mensajes(req):
+    req = request.get_json()
+    add_messages_log(req)
+
+    return jsonify({"message": "EVENT_RECEIVED"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
