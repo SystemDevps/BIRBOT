fc39b713480b368e6231962907e244c55332bb62
SystemDevps <systemeveloperscompany@gmail.com>
2025-03-06 18:13:19 -0600

    SQLALCHEMY

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 
@@ -14,28 +16,34 @@ class Log(db.Model):
     texto = db.Column(db.TEXT)
 
 
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
+
+    prueba1 = Log(texto='Prueba 1')
+    prueba2 = Log(texto='Prueba 2')
+
+    db.session.add(prueba1)
+    db.session.add(prueba2)
+    db.session.commit()
+
+def ordenar_por_fecha_y_hora(registros):
+    return sorted(registros, key=lambda x: x.fecha_y_hora, reverse=True)
 
 @app.route('/')
 def index():
 
     registros = Log.query.all()
+    registros_ordenados = ordenar_por_fecha_y_hora(registros)
+    return render_template('index.html',registros=registros_ordenados)
 
+    messages_log = []
 
-    return render_template('index.html', registros=registros)
-
-    menssage_log = []
-
-    def agregar_message_log(texto):
-        message_log.append(texto)
+    def agregar_messages_log(texto):
+        messages_log.append(texto)
 
         nuevo_registro = Log(texto=texto)
         db.session.add(nuevo_registro)
         db.session.commit()
 
-        agregar_message_log("El usuario ha ingresado a la página de inicio")
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
